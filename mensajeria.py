dd83658634cc1db98fda451d4a95ae5762e51f2e
Adrian Miguelez Benito <amiguelezb@gmail.com>
2025-04-30 19:37:10 +0200

    3. Cambio de math case por un else if

--- mensajeria.py
+++ mensajeria.py
@@ -18,7 +18,7 @@ class SMS(Notificacion):
       super().__init__(mensaje, identificacion, nombre)
 
     def enviar_mensaje(self, num):
-      sms_enviados += 1
+      SMS.sms_enviados += 1
       print(f"Enviando SMS al número {num}...")
 
   
@@ -32,7 +32,7 @@ class Email(Notificacion):
       self.direccion_email = direccion_email
 
     def enviar_mensaje(self):
-      email_enviados += 1
+      Email.email_enviados += 1
       print(f"Enviando email con asunto: {self.asunto} a {self.direccion_email}...")
 
 
@@ -43,17 +43,18 @@ def main():
     identificacion = input("Introduce el codigo de identificacion: ")
     nombre = input("Introduce el nombre de usuario: ")
     
-    match metodo_envio:
-        case "sms":
-            num_telefono = input("¿A qué número de teléfono quieres enviarlo: ")
-            notificacion = SMS(mensaje, identificacion, nombre)
-            notificacion.enviar_mensaje(num_telefono)
 
-        case "email":
-            direccion_email = input("Destinatario: ")
-            asunto = input("Asunto: ")
-            notificacion = Email(mensaje, identificacion, nombre, asunto, direccion_email)
-            notificacion.enviar_mensaje()
+    if metodo_envio == "sms":
+        numero = input("¿A qué número de teléfono quieres enviarlo?: ")
+        notificacion = SMS(mensaje, identificacion, nombre)
+        notificacion.enviar_mensaje(numero)
 
+    elif metodo_envio == "email":
+        direccion_email = input("Destinatario: ")
+        asunto = input("Asunto: ")
+        notificacion = Email(mensaje, identificacion, nombre, asunto, direccion_email)
+        notificacion.enviar_mensaje()
+    else:
+        print("Método de envío no reconocido. Usa 'SMS' o 'Email'.")
 
-main()  
+main()
